c91bf7b33b3ea516ec378979427193e1b576dadb
soazinho <hugues.carlos.soares@gmail.com>
2025-08-05 19:33:42 -0400

    stable version of spring arm camera 3d

--- src/player.cpp
+++ src/player.cpp
@@ -5,11 +5,21 @@
 #include "player.h"
 
 #include <godot_cpp/classes/input.hpp>
+#include <godot_cpp/classes/input_event_mouse_motion.hpp>
 
 using namespace godot;
 
 void Player::_bind_methods() 
 {
+    // Bind exported properties.
+    ClassDB::bind_method(D_METHOD("set_mouse_sensitivity", "sensitivity"), &Player::set_mouse_sensitivity);
+    ClassDB::bind_method(D_METHOD("get_mouse_sensitivity"), &Player::get_mouse_sensitivity);
+    ClassDB::bind_method(D_METHOD("set_tilt_limit", "limit"), &Player::set_tilt_limit);
+    ClassDB::bind_method(D_METHOD("get_tilt_limit"), &Player::get_tilt_limit);
+
+    // Add properties with export ranges.
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "mouse_sensitivity", PROPERTY_HINT_RANGE, "0.0,1.0"), "set_mouse_sensitivity", "get_mouse_sensitivity");
+    ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "tilt_limit"), "set_tilt_limit", "get_tilt_limit");
 }
 
 Player::Player() 
@@ -22,11 +32,31 @@ Player::~Player()
 
 void Player::_ready() 
 {
+    _camera3D = get_node<godot::Camera3D>("CameraPivot/SpringArm3D/Camera3D");
+    _cameraPivot = get_node<Node3D>("CameraPivot");
+}
+
+void Player::_unhandled_input(const Ref<InputEvent> &event) {    
+    Ref<InputEventMouseMotion> motion_event = event;
+    if (motion_event.is_valid()) {
+        // Apply horizontal rotation.
+        Vector3 rotation_degrees = get_rotation_degrees();
+        rotation_degrees.y -= motion_event->get_relative().x * _mouseSensitivity;
+        set_rotation_degrees(rotation_degrees);
+        
+        // Apply vertical rotation.
+        rotation_degrees = _cameraPivot->get_rotation_degrees();
+        rotation_degrees.x -= motion_event->get_relative().y * _mouseSensitivity;
+        rotation_degrees.x = Math::clamp(rotation_degrees.x, -tilt_limit, tilt_limit);
+        _cameraPivot->set_rotation_degrees(rotation_degrees);
+    }
 }
 
 // Update the node at fixed time intervals.
 void Player::_physics_process(double delta) 
 {
+    UtilityFunctions::print("foo!");
+
     Vector3 direction = Vector3();
 
     if (Input::get_singleton()->is_action_pressed("move_right")) 
@@ -66,4 +96,20 @@ void Player::_physics_process(double delta)
     // Moving the character.
     set_velocity(_targetVelocity);
     move_and_slide();
+}
+
+void Player::set_mouse_sensitivity(float p_sensitivity) {
+    _mouseSensitivity = p_sensitivity;
+}
+
+float Player::get_mouse_sensitivity() const {
+    return _mouseSensitivity;
+}
+
+void Player::set_tilt_limit(float p_limit) {
+    tilt_limit = p_limit;
+}
+
+float Player::get_tilt_limit() const {
+    return tilt_limit;
 }
--- src/player.h
+++ src/player.h
@@ -6,14 +6,23 @@
 #define PLAYER_H
 
 #include <godot_cpp/classes/character_body3d.hpp>
+#include <godot_cpp/classes/camera3d.hpp>
+#include <godot_cpp/classes/input_event.hpp>
 
 class Player : public godot::CharacterBody3D 
 {
     GDCLASS(Player, CharacterBody3D)
 
 private:
+    godot::Camera3D* _camera3D;
+    Node3D* _cameraPivot;
+
+    
+    float tilt_limit = godot::Math::deg_to_rad(75.0f);
+    float _mouseSensitivity = 0.3f;
+    
+    float _fallAcceleration = 75.0f;
     float _speed = 5.0f;
-    float _fallAcceleration = 75;
     godot::Vector3 _targetVelocity = godot::Vector3();
 
 protected:
@@ -25,6 +34,13 @@ public:
     
     void _ready() override;
     void _physics_process(double delta) override;
+    void _unhandled_input(const godot::Ref<godot::InputEvent> &event) override;
+
+    float get_mouse_sensitivity() const;
+    void set_mouse_sensitivity(float p_sensitivity);
+
+    float get_tilt_limit() const;
+    void set_tilt_limit(float p_limit);
 };
 
 #endif //PLAYER_H
